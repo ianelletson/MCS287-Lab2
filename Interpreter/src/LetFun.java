996f8f3a47da4a7a4f7d27455c9c2ca4c86fd09d
Ian Elletson <ianelletson@gmail.com>
2014-03-24 10:17:30 -0500

    Init commit

---
+++ Interpreter/src/LetFun.java
@@ -0,0 +1,15 @@
+
+public class LetFun implements Expression {
+
+	public LetFun(String functionName, String parameter, Expression functionBody,
+			Expression letBody) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Value eval(Environment env) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Interpreter/src/LetVal.java
@@ -0,0 +1,14 @@
+
+public class LetVal implements Expression {
+
+	public LetVal(String name, Expression valueExpr, Expression bodyExpr) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Value eval(Environment env) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
