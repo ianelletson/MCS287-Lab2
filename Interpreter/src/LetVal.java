3e65dfa81bb6a709bc803b00ab28f2b95418331b
Ian Elletson <ianelletson@gmail.com>
2014-03-24 11:21:19 -0500

    Still working on ext2

--- Interpreter/src/LetVal.java
+++ Interpreter/src/LetVal.java
@@ -4,22 +4,26 @@ public class LetVal implements Expression {
 	private String name;
 	private Expression value;
 	private Expression body;
+	private ActivationRecord act;
 
 	public LetVal(String name, Expression valueExpr, Expression bodyExpr) {
 		// TODO Auto-generated constructor stub
 		this.name = name;
 		this.value = valueExpr;
 		this.body = bodyExpr;
+//		this.act = new ActivationRecord(name,)
 	}
 
 	@Override
 	public Value eval(Environment env) {
 		// TODO Auto-generated method stub
+		// TODO Do fFive with 6 - 1
+		Value val = body.eval(env);
+//		ActivationRecord act = new ActivationRecord(this.name, val, env);
+//		Value eVal = env.get(name);
 		
-		Value eVal = env.get(name);
 		
-		
-		return null;
+		return val;
 	}
 
 	@Override
